2e6e6f798afd37a0b583b42e7bd5adc0ba7851f8
Wojciech M <wojtekm@myprograme.com>
2021-12-14 12:54:06 +0100

    Removed all files that are in the .gitignore

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from SetofFlashcards import SetOfFlashcards
 from Flashcard import Flashcard
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+UPLOAD_FOLDER = 'Uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('upload.html')
+
+
+def type_check(picture):
+    print("type")
+    if picture.mimetype[:5] == "image":
+        return True
+    else:
+        print(False)
+        return False
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files.getlist('file')
+    for picture in uploaded_file:
+        if picture.filename != "" and type_check(picture):
+            name = picture.filename
+            picture.save(f"Uploads/{name}")
+    # if uploaded_file.filename != '':
+    #     uploaded_file.save(f"Uploads/{uploaded_file.filename}")
+    return redirect(url_for('index'))
 
 
 def itEmpty(list: list[str]) -> bool:
@@ -32,7 +57,12 @@ def test():
         definition = request.form.getlist('definition')
         isempty_concept = itEmpty(concept)
         isempty_definition = itEmpty(definition)
-
+        uploaded_file = request.files.getlist('file')
+        for picture in uploaded_file:
+            if picture.filename != "" and type_check(picture):
+                name = picture.filename
+                print(name)
+                picture.save(f"Uploads/{name}")
         if isempty_concept and isempty_definition:
             for index, value in enumerate(concept):
                 flashcard = Flashcard(value, definition[index])
--- static/index.js
+++ static/index.js
@@ -3,14 +3,21 @@ console.log("Hello World!");
 function add_input(){
     var input = document.createElement("input");
     var input_definition = document.createElement("input");
+    var image = document.createElement("input");
     input.type = "text";
     input.name = "concept";
     input_definition.type = "text";
     input_definition.name = "definition";
+    image.type = "file";
+    image.name = "file";
+    image.accept = "image/*";
     add_label("concept", "Pojęcie");
     document.getElementById("box_form").appendChild(input);
     add_label("definition", "Definicja");
-    document.getElementById("box").appendChild(input_definition);
+    document.getElementById("box_form").appendChild(input_definition);
+    add_label("file", "Obrazek");
+    document.getElementById("box_form").appendChild(image);
+
 }
 
 function add_label(name, name_pl){
